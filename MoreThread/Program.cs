0011eaa0d0864e35b56d27b2e00c40fe22622b70
yangwohenmai <zongxianyang@outlook.com>
2019-05-15 13:36:29 +0800

    update

--- MoreThread/Program.cs
+++ MoreThread/Program.cs
@@ -10,43 +10,29 @@ namespace MoreThread
 {
     class Program
     {
-
-        static SemaphoreSlim semLim = new SemaphoreSlim(3); //3表示最多只能有三个线程同时访问
-
+        static SemaphoreSlim semLim = new SemaphoreSlim(4); //3表示最多只能有三个线程同时访问
 
 
         public static void Main(string[] args)
         {
             for (int i = 0; i < 10; i++)
             {
-                new Thread(SemaphoreTest).Start();
+                new Thread(()=>SemaphoreTest(i)).Start();
             }
             Console.Read();
 
             
-         
         }
 
 
-        static void SemaphoreTest()
+        static void SemaphoreTest(int i)
         {
+            Console.WriteLine("线程" + i + "进入准备111111:" + DateTime.Now.ToString());
             semLim.Wait();
-            Console.WriteLine("线程" + Thread.CurrentThread.ManagedThreadId.ToString() + "开始执行:"+DateTime.Now.ToString());
-            Random rd = new Random();
-            rd.Next(1, 3);
-            Thread.Sleep(rd.Next(3, 55) *1000);
-            Console.WriteLine("线程" + Thread.CurrentThread.ManagedThreadId.ToString() + "执行完毕:" + DateTime.Now.ToString());
+            Console.WriteLine("线程" + i + "开始执行22222222:"+DateTime.Now.ToString());
+            Thread.Sleep((2 + 2 * i) * 1000);
+            Console.WriteLine("线程" + Thread.CurrentThread.ManagedThreadId.ToString() + "执行完毕33333333:" + DateTime.Now.ToString());
             semLim.Release();
         }
-
-
-
-
-
     }
-
-
-
-
-
 }
---
+++ SemaphoreSlim/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SemaphoreSlimTest
+{
+    class Program
+    {
+        static SemaphoreSlim _semaphorSlim = new SemaphoreSlim(4);
+        static Semaphore _semaphor = new Semaphore(2,4);
+        static void Main(string[] args)
+        {
+
+
+            for (int i = 1; i <= 9; i++)
+            {
+                string threadName = "Thread" + i;
+                int secondsToWait = 2 + 2 * i;
+                var t = new Thread(() => _semaphorT(threadName, secondsToWait));
+                t.Start();
+            }
+            Console.ReadKey();
+
+
+
+            for (int i = 1; i <= 9; i++)
+            {
+                string threadName = "Thread" + i;
+                int secondsToWait = 2 + 2 * i;
+                var t = new Thread(() => _semaphorSlimT(threadName, secondsToWait));
+                t.Start();
+            }
+            Console.ReadKey();
+        }
+
+
+        static void _semaphorT(string name, int seconds)
+        {
+            Console.WriteLine("{0} 等待信号量", name);
+            _semaphor.WaitOne();
+            Console.WriteLine("{0} 获得信号量，开始执行", name);
+            Thread.Sleep(1000 * seconds);
+            Console.WriteLine("{0} 结束", name);
+            _semaphor.Release();
+        }
+
+
+
+        static void _semaphorSlimT(string name, int seconds)
+        {
+            Console.WriteLine("{0} 等待信号量", name);
+            _semaphorSlim.Wait();
+            Console.WriteLine("{0} 获得信号量，开始执行", name);
+            //Thread.Sleep(TimeSpan.FromSeconds(seconds));
+            Thread.Sleep(1000* seconds);
+            Console.WriteLine("{0} 结束", name);
+            _semaphorSlim.Release();
+        }
+        
+    }
+}
