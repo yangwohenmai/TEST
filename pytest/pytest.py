a011228203c8baefe8911faa234c45bdef33986a
yangwohenmai <zongxianyang@outlook.com>
2019-11-20 17:35:17 +0800

    update

--- pytest/pytest.py
+++ pytest/pytest.py
@@ -1,188 +1,119 @@
-from pandas import DataFrame
-from pandas import Series
-from pandas import concat
+from math import sqrt
+from numpy import concatenate
+from matplotlib import pyplot
 from pandas import read_csv
-from pandas import datetime
-from sklearn.metrics import mean_squared_error
+from pandas import DataFrame
+from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import LabelEncoder
+from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
-from math import sqrt
-from matplotlib import pyplot
-from numpy import array
+import pandas as pd
 
-# date-time parsing function for loading the dataset
-def parser(x):
-	return datetime.strptime('190'+x, '%Y-%m')
+pd.set_option('display.max_columns',1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_colwidth',1000)
 
-# convert time series into supervised learning problem
+ 
+# convert series to supervised learning
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-	n_vars = 1 if type(data) is list else data.shape[1]
-	df = DataFrame(data)
-	cols, names = list(), list()
-	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
-		cols.append(df.shift(-i))
-		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-	# put it all together
-	agg = concat(cols, axis=1)
-	agg.columns = names
-	# drop rows with NaN values
-	if dropnan:
-		agg.dropna(inplace=True)
-	return agg
-
-# 差分算法
-def difference(dataset, interval=1):
-	diff = list()
-	for i in range(interval, len(dataset)):
-		value = dataset[i] - dataset[i - interval]
-		diff.append(value)
-	return Series(diff)
-
-# 数据特征工程
-def prepare_data(series, n_test, n_lag, n_seq):
-    # 提取文本中的数据
-    raw_values = series.values
-    # 对数据进行差分计算
-    diff_series = difference(raw_values, 1)
-    # 提取差分后的数据
-    diff_values = diff_series.values
-    print(diff_values)
-    # 重构成n行一列的数据
-    diff_values = diff_values.reshape(len(diff_values), 1)
-    print(diff_values)
-    # 定义数据缩放在(-1，1)之间
-    scaler = MinMaxScaler(feature_range=(-1, 1))
-    print(scaler)
-    # 对数据进行缩放
-    scaled_values = scaler.fit_transform(diff_values)
-    print(scaled_values)
-    # 将缩放后的数据重构成n行一列的数据
-    scaled_values = scaled_values.reshape(len(scaled_values), 1)
-    print(scaled_values)
-    # 将数据构建成步长为n_seq的监督学习型数据
-    supervised = series_to_supervised(scaled_values, n_lag, n_seq)
-    print(supervised)
-    supervised_values = supervised.values
-    # 将数据分割出n_test条作为测试数据
-    train, test = supervised_values[0:-n_test], supervised_values[-n_test:]
-    return scaler, train, test
-
-# 你和一个LSTM网络，训练数据
-def fit_lstm(train, n_lag, n_seq, n_batch, nb_epoch, n_neurons):
-	# 重构训练数据结构-》[samples, timesteps, features]
-	X, y = train[:, 0:n_lag], train[:, n_lag:]
-	X = X.reshape(X.shape[0], 1, X.shape[1])
-	# 网络结构
-	model = Sequential()
-	model.add(LSTM(n_neurons, batch_input_shape=(n_batch, X.shape[1], X.shape[2]), stateful=True))
-	model.add(Dense(y.shape[1]))
-	model.compile(loss='mean_squared_error', optimizer='adam')
-	# 开始训练
-	for i in range(nb_epoch):
-		model.fit(X, y, epochs=1, batch_size=n_batch, verbose=0, shuffle=False)
-		model.reset_states()
-	return model
-
-# make one forecast with an LSTM,
-def forecast_lstm(model, X, n_batch):
-	# reshape input pattern to [samples, timesteps, features]
-	X = X.reshape(1, 1, len(X))
-	# make forecast
-	forecast = model.predict(X, batch_size=n_batch)
-	# convert to array
-	return [x for x in forecast[0, :]]
-
-# evaluate the persistence model
-def make_forecasts(model, n_batch, train, test, n_lag, n_seq):
-	forecasts = list()
-	for i in range(len(test)):
-		X, y = test[i, 0:n_lag], test[i, n_lag:]
-		# 预测
-		forecast = forecast_lstm(model, X, n_batch)
-		# 存储预测数据
-		forecasts.append(forecast)
-	return forecasts
-
-# invert differenced forecast
-def inverse_difference(last_ob, forecast):
-	# invert first forecast
-	inverted = list()
-	inverted.append(forecast[0] + last_ob)
-	# propagate difference forecast using inverted first value
-	for i in range(1, len(forecast)):
-		inverted.append(forecast[i] + inverted[i-1])
-	return inverted
-
-# inverse data transform on forecasts
-def inverse_transform(series, forecasts, scaler, n_test):
-	inverted = list()
-	for i in range(len(forecasts)):
-		# create array from forecast
-		forecast = array(forecasts[i])
-		forecast = forecast.reshape(1, len(forecast))
-		# 数据逆缩放
-		inv_scale = scaler.inverse_transform(forecast)
-		inv_scale = inv_scale[0, :]
-		# 数据逆差分
-		index = len(series) - n_test + i - 1
-		last_ob = series.values[index]
-		inv_diff = inverse_difference(last_ob, inv_scale)
-		# 存储转换后的数据
-		inverted.append(inv_diff)
-	return inverted
-
-# evaluate the RMSE for each forecast time step
-def evaluate_forecasts(test, forecasts, n_lag, n_seq):
-	for i in range(n_seq):
-		actual = [row[i] for row in test]
-		predicted = [forecast[i] for forecast in forecasts]
-		rmse = sqrt(mean_squared_error(actual, predicted))
-		print('t+%d RMSE: %f' % ((i+1), rmse))
-
-# plot the forecasts in the context of the original dataset
-def plot_forecasts(series, forecasts, n_test):
-	# plot the entire dataset in blue
-	pyplot.plot(series.values)
-	# plot the forecasts in red
-	for i in range(len(forecasts)):
-		off_s = len(series) - n_test + i - 1
-		off_e = off_s + len(forecasts[i]) + 1
-		xaxis = [x for x in range(off_s, off_e)]
-		yaxis = [series.values[off_s]] + forecasts[i]
-		pyplot.plot(xaxis, yaxis, color='red')
-	# show the plot
-	pyplot.show()
+    n_vars = 1 if type(data) is list else data.shape[1]
+    df = DataFrame(data)
+    print(df)
+    cols, names = list(), list()
+    # input sequence (t-n, ... t-1)
+    for i in range(n_in, 0, -1):
+        cols.append(df.shift(i))
+        print(cols)
+        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+    # forecast sequence (t, t+1, ... t+n)
+    for i in range(0, n_out):
+        cols.append(df.shift(-i))
+        print(cols)
+        if i == 0:
+        	names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+        else:
+        	names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+    # put it all together
+    print(cols)
+    agg = concat(cols, axis=1)
+    print(agg)
+    agg.columns = names
+    print(agg)
+    # drop rows with NaN values
+    if dropnan:
+    	agg.dropna(inplace=True)
+    return agg
 
 # load dataset
-series = read_csv('shampoo-sales.csv', header=0, parse_dates=[0], index_col=0, squeeze=True, date_parser=parser)
-# configure
-n_lag = 1
-n_seq = 3
-n_test = 10
-n_epochs = 1500
-n_batch = 1
-n_neurons = 1
-# 数据差分，缩放，重构成监督学习型数据
-scaler, train, test = prepare_data(series, n_test, n_lag, n_seq)
-# 拟合模型
-model = fit_lstm(train, n_lag, n_seq, n_batch, n_epochs, n_neurons)
-# 开始预测
-forecasts = make_forecasts(model, n_batch, train, test, n_lag, n_seq)
-# inverse transform forecasts and test
-forecasts = inverse_transform(series, forecasts, scaler, n_test+2)
-actual = [row[n_lag:] for row in test]
-actual = inverse_transform(series, actual, scaler, n_test+2)
-# evaluate forecasts
-evaluate_forecasts(actual, forecasts, n_lag, n_seq)
-# plot forecasts
-plot_forecasts(series, forecasts, n_test+2)
+dataset = read_csv('pollution.csv', header=0, index_col=0)
+values = dataset.values
+print(values)
+# 对第四列“风向”进行数字编码转换
+encoder = LabelEncoder()
+values[:,4] = encoder.fit_transform(values[:,4])
+print(values[:,4])
+# 数据转换为浮点型
+values = values.astype('float32')
+# 将所有数据缩放到（0，1）之间
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled = scaler.fit_transform(values)
+# 将数据格式化成监督学习型数据
+reframed = series_to_supervised(scaled, 1, 1)
+print(reframed)
+# 删掉那些我们不想预测的列
+reframed.drop(reframed.columns[[9,10,11,12,13,14,15]], axis=1, inplace=True)
+print(reframed.head())
+ 
+# split into train and test sets
+values = reframed.values
+print(values)
+# 取出一年的数据作为训练数据，剩下的做测试数据
+n_train_hours = 365 * 24
+train = values[:n_train_hours, :]
+test = values[n_train_hours:, :]
+# 将数据分割成输入和输出，最后一列数据作为输出数据
+train_X, train_y = train[:, :-1], train[:, -1]
+test_X, test_y = test[:, :-1], test[:, -1]
+# 将输入数据转换成3D张量 [samples, timesteps, features]
+train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+ 
+# 设计网络结构
+model = Sequential()
+model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(Dense(1))
+model.compile(loss='mae', optimizer='adam')
+# 拟合网络
+history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+# 图像展示训练损失
+#pyplot.plot(history.history['loss'], label='train')
+#pyplot.plot(history.history['val_loss'], label='test')
+#pyplot.legend()
+#pyplot.show()
+ 
+# 使用拟合后的网络进行预测
+yhat = model.predict(test_X)
+print(yhat[0:30,:])
+print(test_X[0:30,:])
+# xx = test_X[0:35000,:].reshape((17500,2 ,8))
+# print(xx)
+# 将3D转换为2D
+test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+print(inv_yhat)
+# 对预测数据逆缩放
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# 计算RMSE值
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
