d1f537fed3ae103b90ce3e2f71864143d580f7b9
yangwohenmai <zongxianyang@outlook.com>
2020-01-02 10:22:11 +0800

    多线程

--- TaskSchedulerTest/Program.cs
+++ TaskSchedulerTest/Program.cs
@@ -9,12 +9,17 @@ namespace TaskSchedulerTest
     {
         static void Main(string[] args)
         {
-            TaskTest3();
+            //线程调度测试
+            //TaskTest1();
+
             //效果更好
             //TaskTest2();
 
-            //线程调度测试
-            //TaskTest1();
+            //解决TaskTest2中创建线程缓慢的问题
+            //TaskTest3();
+
+            //解决TaskTest2中创建线程缓慢的问题
+            TaskTest4();
 
             //线程调度器
             //TaskScheduler();
@@ -81,7 +86,6 @@ namespace TaskSchedulerTest
             //TaskFactory fac = new TaskFactory();
             for (int i = 0; i < 1000; i++)
             {
-
                 fac.StartNew(s =>
                 {
                     //Thread.Sleep(1000);
@@ -103,7 +107,6 @@ namespace TaskSchedulerTest
         /// </summary>
         public static void TaskTest2()
         {
-
             //限制同时只有5个并发线程
             TaskFactory fac = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(10));
 
@@ -125,73 +128,86 @@ namespace TaskSchedulerTest
                 }, i);
             }
             Console.ReadLine();
-
-
-            
         }
 
+        /// <summary>
+        /// 通过设置SetMinThreads，解决TaskFactory启动线程缓慢的问题
+        /// </summary>
         public static void TaskTest3()
         {
-            //var hp = new HttpWebRequestTest.HttpRequestClient();
-            //string reslut = hp.httpGet("https://www.hkex.com.hk/?sc_lang=EN", HttpWebRequestTest.HttpRequestClient.defaultHeaders);
-            ////定位token字符串头
-            //int index_head = reslut.IndexOf("evLtsLs");
-            //if (index_head == -1)
-            //{
-            //    //LogUtil.Error("Common", "MainFrom", "DoWork", "获取token失败");
-            //    Console.WriteLine("获取token失败");
-            //    //进入失败队列
+            ThreadPool.SetMinThreads(20, 1000);
+            //限制同时只有5个并发线程
+            TaskFactory fac = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(10));
 
-            //}
-            //string InitToken = reslut.Substring(index_head, 100);
-            ////定位token字符串尾
-            //int index_last = InitToken.IndexOf('"');
-            ////截取token
-            //string Token = reslut.Substring(index_head, index_last);
-            //string list = @"00001,00002,00003,00004,00005,00006,00007,00008,00009,00010,00011,00012,00014,00015,00016,00017,00018,00019,00020,00021,00022,00023,00024,00025,00026,00027,00028,00029,00030,00031,00032,00033,00034,00035,00036,00037,00038,00039,00040,00041,00042,00043,00045,00046,00047,00048,00050,00051,00052,00053,00055,00056,00057,00058,00059,00060,00061,00062,00063,00064,00065,00066,00067,00068,00069,00070,00071,00072,00073,00075,00076,00077,00078,00079,00080,00081,00082,00083,00084,00085,00086,00087,00088,00089,00090,00091,00092,00093,00094,00095,00096,00097,00098,00099,00100,00101,00102,00103,00104,00105,00106,00107,00108,00109,00110,00111,00112,00113,00114,00115,00116,00117,00118,00119,00120,00122,00123,00124,00125,00126,00127,00128,00129,00130,00131,00132,00133,00135,00136,00137,00138,00139,00141,00142,00143,00144,00145,00146,00147,00148,00149,00150,00151,00152,00153,00154,00155,00156,00157,00158,00159,00160,00161,00162,00163,00164,00165,00166,00167,00168,00169,00171,00172,00173,00174,00175,00176,00177,00178,00179,00180,00181,00182,00183,00184,00185,00186,00187,00188,00189,00190,00191,00193,00194,00195,00196,00197,00198,00199,00200,00201,00202,00204,00205,00206,00207,00208,00209,00210,00211,00212,00213,00214,00215,00216,00217,00218,00219,00220,00221,00222,00223,00224,00225,00226,00227,00228,00229,00230,00231,00232,00233,00234,00235,00236,00237,00238";
+            //TaskFactory fac = new TaskFactory();
+            //总共创建1000个线程，所有线程进入线程池等待
+            for (int i = 0; i < 1000; i++)
+            {
+                //向线程池中添加一个线程
+                fac.StartNew(s =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("启动线程：" + s);
+                    //b++;
+                    //每个线程存活20秒，当前线程结束后，线程池会从新分配5个线程
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Current Index {0}, ThreadId {1}", s, Thread.CurrentThread.ManagedThreadId);
+                    Console.WriteLine("结束线程：" + s);
 
-            //string[] strlist = list.Split(',');
-            //TaskFactory fac = new TaskFactory(new LimitedConcurrencyLevelTaskScheduler(20));
-            //Task[] tasks = new Task[strlist.Length];
-            //for (int i = 0; i < strlist.Length; i++)
-            //{
-            //    tasks[i] = fac.StartNew(s =>
-            //    {
-            //        //Console.WriteLine(strlist[i] + "开始执行");
-            //        string link = string.Format("https://www1.hkex.com.hk/hkexwidget/data/getequityquote?sym={0}&token={1}&lang=eng&qid=NULL&callback=0",
-            //    Convert.ToInt32(strlist[i]), Token);
-            //        //从港交所接口获取数据
-            //        var hp1 = new HttpWebRequestTest.HttpRequestClient();
-            //        string data = hp1.httpGet(link, HttpWebRequestTest.HttpRequestClient.defaultHeaders);
-            //        Console.WriteLine(strlist[i] + "执行完成");
-            //    }, i);
-
-            //}
+                }, i);
+            }
+            Console.ReadLine();
+        }
 
 
+        /// <summary>
+        /// 通过设置TaskFactory参数，可以加速线程启动过程
+        /// 但此时无法再设置线程最大并发数量
+        /// </summary>
+        public static void TaskTest4()
+        {
+            TaskFactory fac = new TaskFactory(TaskCreationOptions.LongRunning, TaskContinuationOptions.LongRunning);
+            //TaskFactory fac = new TaskFactory();
+            //总共创建1000个线程，所有线程进入线程池等待
+            for (int i = 0; i < 1000; i++)
+            {
+                //向线程池中添加一个线程
+                fac.StartNew(s =>
+                {
+                    Thread.Sleep(1000);
+                    Console.WriteLine("启动线程：" + s);
+                    //b++;
+                    //每个线程存活20秒，当前线程结束后，线程池会从新分配5个线程
+                    Thread.Sleep(10000);
+                    Console.WriteLine("Current Index {0}, ThreadId {1}", s, Thread.CurrentThread.ManagedThreadId);
+                    Console.WriteLine("结束线程：" + s);
+
+                }, i);
+            }
+            Console.ReadLine();
         }
 
 
 
 
 
-            //private static void Write_txt(string log)
-            //{
-            //    lock (b1)
-            //    {
-            //        string path = "D:\\YSJS-BK-Finance\\logs1\\";//文件路径
-            //        string logFileName = Path.Combine(path, DateTime.Now.ToString("yyyyMMdd") + "PEQ4004BLError.log");//全部路径
-            //        if (!Directory.Exists(path))//若文件夹不存在则新建文件夹   
-            //            Directory.CreateDirectory(path); //新建文件夹   
+        //private static void Write_txt(string log)
+        //{
+        //    lock (b1)
+        //    {
+        //        string path = "D:\\YSJS-BK-Finance\\logs1\\";//文件路径
+        //        string logFileName = Path.Combine(path, DateTime.Now.ToString("yyyyMMdd") + "PEQ4004BLError.log");//全部路径
+        //        if (!Directory.Exists(path))//若文件夹不存在则新建文件夹   
+        //            Directory.CreateDirectory(path); //新建文件夹   
 
-            //        File.AppendAllText(logFileName, DateTime.Now.ToString() + " ");
-            //        File.AppendAllText(logFileName, log);
-            //        File.AppendAllText(logFileName, Convert.ToChar(13).ToString());
-            //        File.AppendAllText(logFileName, Convert.ToChar(10).ToString());
-            //    }
+        //        File.AppendAllText(logFileName, DateTime.Now.ToString() + " ");
+        //        File.AppendAllText(logFileName, log);
+        //        File.AppendAllText(logFileName, Convert.ToChar(13).ToString());
+        //        File.AppendAllText(logFileName, Convert.ToChar(10).ToString());
+        //    }
 
 
-            //}
+        //}
 
-        }
+    }
 }
