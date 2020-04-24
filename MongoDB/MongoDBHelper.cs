333faeff9a8c52390a5fd6b7d151002f92c88e16
yangwohenmai <zongxianyang@outlook.com>
2020-04-24 08:53:22 +0800

    update

--- MongoDB/MongoDBHelper.cs
+++ MongoDB/MongoDBHelper.cs
@@ -1,10 +1,60 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace MongoDB
 {
-    class MongoDBHelper
+    class MongoDBHelper<T>
     {
+        private IMongoCollection<T> collection = null;
+        /// <summary>
+        /// 更新
+        /// </summary>
+        /// <param name="entity"></param>
+        public bool Update(Expression<Func<T, bool>> express, T entity)
+        {
+            try
+            {
+                var old = collection.Find(express).ToList().FirstOrDefault();
+
+                foreach (var prop in entity.GetType().GetProperties())
+                {
+                    if (!prop.Name.Equals("_id"))
+                    {
+                        var newValue = prop.GetValue(entity);
+                        var oldValue = old.GetType().GetProperty(prop.Name).GetValue(old);
+
+                        if (newValue != null)
+                        {
+                            if (oldValue == null)
+                                oldValue = "";
+                            if (!newValue.ToString().Equals(oldValue.ToString()))
+                            {
+                                old.GetType().GetProperty(prop.Name).SetValue(old, newValue.ToString());
+                            }
+                        }
+                    }
+                }
+
+                // var filter = Builders<T>.Filter.Eq("Id", entity.Id);
+                ReplaceOneResult result = collection.ReplaceOneAsync(express, old).Result;
+                if (result.ModifiedCount > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                var aaa = ex.Message + ex.StackTrace;
+                throw;
+            }
+        }
     }
 }
--- MongoDB/Program.cs
+++ MongoDB/Program.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MongoDB
 {
     class Program
     {
+        static IMongoDatabase Database = new MongoClient("mongodb://finchina:finchina@10.15.97.183:27017/DerivedData").GetDatabase("DerivedData");
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            List<WriteModel<BsonDocument>> InputDBList = new List<WriteModel<BsonDocument>>();
+            IMongoCollection<BsonDocument> hk_test = Database.GetCollection<BsonDocument>("hk_test");
+
+
+
+            var filter1 = Builders<BsonDocument>.Filter.Ne("ID", 11);
+            var result = Database.GetCollection<BsonDocument>("hk_test").Find(filter1).ToList();
+            var rep = new ReplaceOneModel<BsonDocument>(filter1, new BsonDocument("ID", 14).Add("ITCode2", "ok").Add("TMSTAMP","OK"));
+            rep.IsUpsert = true;
+            InputDBList.Add(rep);
+            hk_test.BulkWrite(InputDBList);
+
+
+
+
+
+            var filter = Builders<BsonDocument>.Filter.Eq("ID", 11);
+            //var update = Builders<BsonDocument>.Update.Set("ITCode2", "OK2").Set("TMSTAMP", "OK2");
+            var update = Builders<BsonDocument>.Update.Set("ITCode2", "OK2");
+            Database.GetCollection<BsonDocument>("hk_test").UpdateMany("{}", update);
+            //Database.GetCollection<BsonDocument>("hk_test").UpdateMany(filter, update, new UpdateOptions { IsUpsert = true});
+            //var updateOptions = new UpdateOptions { IsUpsert = true };
+            //InputDBList.Add(new UpdateManyModel<BsonDocument>(filter, update));
+            var a = new UpdateManyModel<BsonDocument>(filter, update);
+            a.IsUpsert = true;
+            InputDBList.Add(a);
+            hk_test.BulkWrite(InputDBList);
+
+
+            //var projection = Builders<BsonDocument>.Projection.Include("ID").Include("ITCode2").Include("TMSTAMP").Exclude("_id");
+            //var result = Database.GetCollection<BsonDocument>("hk_test").Find(new BsonDocument()).Project(projection).ToList().ToJson();
+            ////var document2 = Database.GetCollection<BsonDocument>("TCR0002_V2").Find(new BsonDocument()).Project(projection).ToList().ToJson();
+            //var DataList = BsonSerializer.Deserialize<List<BsonDocument>>(result).AsQueryable().ToList();
         }
+
+
+
     }
 }
